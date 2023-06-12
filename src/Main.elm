e66f682af57154eaff798e609dbe9c464458b9b1
Josh M <20140997+jcmrva@users.noreply.github.com>
2023-06-11 21:01:37 -0400

    clicable url

--- src/Main.elm
+++ src/Main.elm
@@ -2,9 +2,8 @@ module Main exposing (main)
 
 import Browser
 import Html exposing (..)
-import Html.Events exposing (onInput)
-import Html.Attributes exposing (value, type_, checked, id, for, size)
-import Html.Events exposing (onClick)
+import Html.Attributes exposing (value, type_, checked, id, for, size, href)
+import Html.Events exposing (onInput, onClick)
 import Url
 
 main : Program () Model Msg
@@ -21,19 +20,20 @@ type alias Model =
     , decodedUrl : Maybe String
     , noSafelinks : Bool
     , showComponents : Bool
-    , x : String
+    , clickable : Bool
     }
 
-init : () -> ( Model, Cmd Msg )
+init : () -> (Model, Cmd Msg)
 init _ =
-    ({ inputUrl = "", decodedUrl = Nothing, x = "", noSafelinks = False, showComponents = False }, Cmd.none)
+    ({ inputUrl = "", decodedUrl = Nothing, noSafelinks = False, clickable = False, showComponents = False }, Cmd.none)
 
 type Msg
     = UrlUpdate String
     | ToggleSafelinks
     | ToggleComponents
+    | ToggleClickableUrl
 
-update : Msg -> Model -> ( Model, Cmd Msg )
+update : Msg -> Model -> (Model, Cmd Msg)
 update msg model =
     let
         decoded noSF url = (if noSF then removeSafelink url else url) |> Url.percentDecode
@@ -48,6 +48,8 @@ update msg model =
             ({ model | noSafelinks = sf, decodedUrl = decoded sf model.inputUrl }, Cmd.none)
         ToggleComponents ->
             ({ model | showComponents = not model.showComponents }, Cmd.none)
+        ToggleClickableUrl ->
+            ({ model | clickable = not model.clickable }, Cmd.none)
 
 type alias Document msg =
     { title : String
@@ -58,44 +60,50 @@ view : Model -> Document Msg
 view model =
     { title = "URL Decoder"
     , body =
-        [ div [ ]
-            [
-              input [ type_ "checkbox", checked model.noSafelinks, onClick ToggleSafelinks, id "safelinks" ] []
+        [ div []
+            [ input [ type_ "checkbox", checked model.noSafelinks, onClick ToggleSafelinks, id "safelinks" ] []
             , label [ for "safelinks" ] [ text "Remove Safe Links" ]
-            , input [ type_ "checkbox", checked model.showComponents, onClick ToggleComponents, id "components" ] [ ]
+            , input [ type_ "checkbox", checked model.showComponents, onClick ToggleComponents, id "components" ] []
             , label [ for "components" ] [ text "Show Components" ]
+            , input [ type_ "checkbox", checked model.clickable, onClick ToggleClickableUrl, id "clickable" ] []
+            , label [ for "clickable" ] [ text "Clickable Decoded URL" ]
             , br [] []
             , input [ value model.inputUrl, onInput UrlUpdate, size 100 ] []
-            , p [ ] [ model.decodedUrl |> Maybe.withDefault "" |> text ]
-            , p [ ] [ if model.showComponents then (model.decodedUrl |> (Maybe.andThen Url.fromString) |> urlDisplay) else text "" ]
+            , p [] [ formatDecodedUrl model.clickable model.decodedUrl ]
+            , p [] [ if model.showComponents then (model.decodedUrl |> (Maybe.andThen Url.fromString) |> urlDisplay) else text "" ]
             ]
         ]
     }
 
+formatDecodedUrl : Bool -> Maybe String -> Html msg
+formatDecodedUrl clickable decodedUrl =
+    case (clickable, decodedUrl) of
+    (True, Just url) ->
+        a [ href url ] [ url |> text ]
+    _ ->
+        decodedUrl |> Maybe.withDefault "" |> text
+
 removeSafelink : String -> String
 removeSafelink url =
-    if String.contains "safelinks" url then
-        let
-            idx = String.indexes "url=" url
-        in
-            case idx of
-            i :: _ ->
-                String.dropLeft (i + 4) url
-            [] ->
-                url
-    else
-        url
+    let
+        idxs = String.indexes "url=" url
+    in
+        case idxs of
+        idx :: _ ->
+            String.dropLeft (idx + 4) url
+        [] ->
+            url
 
 urlDisplay : Maybe Url.Url -> Html Msg
 urlDisplay url =
     case url of
         Just u ->
             ul []
-                [ li [] [ "protocol: " ++ (u.protocol |> protocolString) |> text]
-                , li [] [ "host: " ++ u.host |> text]
-                , li [] [ "path: " ++ u.path |> text]
-                , li [] [ "query: " ++ (u.query |> Maybe.withDefault "") |> text]
-                , li [] [ "fragment: " ++ (u.fragment |> Maybe.withDefault "") |> text]
+                [ li [] [ "protocol: " ++ (u.protocol |> protocolString) |> text ]
+                , li [] [ "host: " ++ u.host |> text ]
+                , li [] [ "path: " ++ u.path |> text ]
+                , li [] [ "query: " ++ (u.query |> Maybe.withDefault "") |> text ]
+                , li [] [ "fragment: " ++ (u.fragment |> Maybe.withDefault "") |> text ]
             ]
         Nothing ->
             "invalid url" |> text
